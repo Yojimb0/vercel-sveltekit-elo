cfec0c7f7e2f0fb31c917afc924dfd78b97ea0f2
Pierre-Yves Pointereau <pypointereau+web@gmail.com>
2023-06-23 04:30:08 +0000

    Adding ELO score

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
   // Import the Firebase SDK and initialize the Firestore database
   import { initializeApp } from 'firebase/app';
   import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
+  import { onSnapshot, doc, updateDoc } from "firebase/firestore";
 
   const firebaseConfig = {
   apiKey: "AIzaSyCBOYHYzC2DYlk2OUT8QDCI_19RJcoqYjk",
@@ -34,13 +35,74 @@ const db = getFirestore(app);
 
       // Display a success message or perform any other action
       console.log("Names stored successfully!");
+
+	  updateEloScores();
     } catch (error) {
       console.error("Error storing names:", error);
     }
   };
 
+  let playerStats = [];
+
+  const calculateElo = (winnerScore, loserScore, kFactor) => {
+    const expectedScoreWinner = 1 / (1 + 10 ** ((loserScore - winnerScore) / 400));
+    const expectedScoreLoser = 1 - expectedScoreWinner;
+
+    const updatedWinnerScore = winnerScore + kFactor * (1 - expectedScoreWinner);
+    const updatedLoserScore = loserScore + kFactor * (0 - expectedScoreLoser);
+
+    return { updatedWinnerScore, updatedLoserScore };
+  };
+
+  const updateEloScores = async () => {
+    try {
+      // Retrieve the matches from the "names" collection
+      const matchesSnapshot = await getDocs(collection(db, "names"));
+      const matches = matchesSnapshot.docs.map((doc) => doc.data());
+
+      // Retrieve the players from the "players" collection
+      const playersSnapshot = await getDocs(collection(db, "players"));
+      const players = playersSnapshot.docs.map((doc) => doc.data());
+
+      // Calculate and update the ELO scores for each player
+      const updatedPlayers = players.map((player) => {
+        let { eloScore } = player;
+
+        for (const match of matches) {
+          if (match.winner === player.name) {
+            const loser = players.find((p) => p.name === match.loser);
+            const { updatedWinnerScore } = calculateElo(eloScore, loser?.eloScore, 32);
+            eloScore = updatedWinnerScore;
+          } else if (match.loser === player.name) {
+            const winner = players.find((p) => p.name === match.winner);
+            const { updatedLoserScore } = calculateElo(eloScore, winner?.eloScore, 32);
+            eloScore = updatedLoserScore;
+          }
+        }
+
+        return { ...player, eloScore };
+      });
+
+      // Update the ELO scores in the "players" collection
+      for (const updatedPlayer of updatedPlayers) {
+        const playerDocRef = doc(db, "players", updatedPlayer.id);
+        await updateDoc(playerDocRef, { eloScore: updatedPlayer.eloScore });
+      }
+
+      // Display a success message or perform any other action
+      console.log("ELO scores updated successfully!");
+    } catch (error) {
+      console.error("Error updating ELO scores:", error);
+    }
+  };
+
   onMount(() => {
-    // Optional: Perform any additional setup or data retrieval here
+    // Retrieve the player statistics from the Firestore collection
+    const unsubscribe = onSnapshot(collection(db, "players"), (snapshot) => {
+      playerStats = snapshot.docs.map((doc) => doc.data());
+    });
+
+    return unsubscribe;
   });
 </script>
 
